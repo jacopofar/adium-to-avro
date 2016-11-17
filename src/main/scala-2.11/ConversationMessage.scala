86d1b34c59ea9d90af8e3c631e603e497c23fcfa
jacopo_farina <jacopo.farina@spindox.it>
2016-11-17 20:19:27 +0100

    place AVSC under version control

--- src/main/scala-2.11/ConversationMessage.scala
+++ src/main/scala-2.11/ConversationMessage.scala
@@ -1,3 +1,5 @@
 import org.joda.time.DateTime
-
+/**
+* A message in a conversation. Field names are pretty much what the names suggest
+  * */
 case class ConversationMessage(body: String, sender: String, channel: String, timestamp: DateTime) {}
