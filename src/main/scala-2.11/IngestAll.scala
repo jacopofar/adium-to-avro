f7eaff39828f5dd3c700b372126f6cb5e1d2ddbc
jacopo_farina <jacopo.farina@spindox.it>
2016-11-17 20:12:15 +0100

    ingest all messages with a comment

--- src/main/scala-2.11/IngestAll.scala
+++ src/main/scala-2.11/IngestAll.scala
@@ -1,18 +1,39 @@
-import java.io.FileWriter
+import java.io.{File, FileWriter}
 
+import org.apache.avro.Schema
+import org.apache.avro.file.DataFileWriter
+
+import org.apache.avro.generic.{GenericData, GenericDatumWriter, GenericRecord}
 import org.joda.time.format.ISODateTimeFormat
 
-/**
-  * Created by utente on 2016-08-30.
-  */
+import scala.collection.JavaConversions.asJavaCollection
+
 object IngestAll {
-  val dtf = ISODateTimeFormat.dateTime();
-  val fw = new FileWriter("all_messages.tsv")
+  val dtf = ISODateTimeFormat.dateTime()
+  val fw = new FileWriter("adium_chat_messages.tsv")
 
   def main(args: Array[String]): Unit = {
+    val schema = new Schema.Parser().parse(new File("corpus_utterance.avsc"))
+    val file = new File("adium_chat_messages.avro")
+    val datumWriter = new GenericDatumWriter[GenericRecord](schema)
+    val dataFileWriter = new DataFileWriter[GenericRecord](datumWriter)
+    dataFileWriter.create(schema, file)
+
     AdiumIngester.ingest((msg: ConversationMessage) => {
-      fw.write(msg.channel + "\t" + dtf.print(msg.timestamp) + "\t" + msg.sender + "\t" + msg.body.replace("\t", " ").replace("\n", " ") + "\n")
+      val utterance = new GenericData.Record(schema)
+      val text = msg.body.replace("\t", " ").replace("\n", " ")
+      val ts = dtf.print(msg.timestamp)
+      utterance.put("text", text)
+      utterance.put("timestamp", ts)
+      utterance.put("source", msg.channel)
+      //Avro serialization of arrays is a bit tricky
+      val tagList = asJavaCollection(List[String](msg.sender, msg.channel))
+      utterance.put("tags",   tagList)
+      dataFileWriter.append(utterance)
+      fw.write(msg.channel + "\t" + ts + "\t" + msg.sender + "\t" + text + "\n")
     })
+
+    dataFileWriter.close()
     fw.close()
   }
 }
